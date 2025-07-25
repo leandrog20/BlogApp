bb746d29714650fa5f1dfd2cb75f71a46ffe3bd9
Leandro Gomes <107895852+leandrog20@users.noreply.github.com>
2025-07-25 18:01:35 -0300

    Remoção das alterações feitas no código

--- routes/usuario.js
+++ routes/usuario.js
@@ -43,8 +43,7 @@ router.post('/registro', (req, res) => {
                 const novoUsuario = new Usuario({
                     nome: req.body.nome,
                     email: req.body.email,
-                    senha: req.body.senha,
-                    eAdmin: 1
+                    senha: req.body.senha
                 })
 
                 const salt = bcrypt.genSaltSync(10);
